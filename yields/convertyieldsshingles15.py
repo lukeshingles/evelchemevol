4fe31a1ea351ba832d845942267f3fbc24926499
Luke Shingles <luke.shingles@gmail.com>
2016-03-11 12:00:09 +0000

    add shingles et al. 2015 yields and update converter

--- yields/convertyieldsshingles15.py
+++ yields/convertyieldsshingles15.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python3
 from collections import OrderedDict
 import struct
+import sys
 
 elements = ['']+[line.split()[1] for line in open('data/atomic_symbols.dat')]
 
-#(yval, k14alpha, s15alpha) = ('24', '4', '0')
-(yval, k14alpha, s15alpha) = ('40', '0', '0')
+#(yval, k14alpha, s15alpha) = ('25', '4', '0') #Helium normal
+(yval, k14alpha, s15alpha) = ('40', '0', '0') #Helium rich
 
-modelNames = [s.format(yval,k14alpha,s15alpha) for s in ['m1.7z0006y{0}a{1}pmz001s320','m2.36z0006y{0}a{1}pmz001s320','m3z0006y{0}a{2}pmz001s320','m4z0006y{0}a{2}s320','m5z0006y{0}a{2}s320','m6z0006y{0}a{2}s320']]
+modelNames = [s.format(yval,k14alpha,s15alpha) for s in [
+                        'm1.7z0006y{0}a{1}pmz001s320',
+                        'm2.36z0006y{0}a{1}pmz001s320',
+                        'm3z0006y{0}a{2}pmz001s320',
+                        'm4z0006y{0}a{2}s320',
+                        'm5z0006y{0}a{2}s320',
+                        'm6z0006y{0}a{2}s320']]
 yields = [OrderedDict({}) for k in range(len(modelNames))]
 ejectaMass = [0.0 for k in range(len(modelNames))]
 initMass = [float(modelNames[k][1:].split('z')[0]) for k in range(len(modelNames))]
 lifetime = [0.0 for k in range(len(modelNames))]
 metallicity = [0.0 for k in range(len(modelNames))]
 
-customlifetimes = {'m1.7z0006y24': 1.4355016E+09, 'm1.7z0006y40': 5.6321442E+08, 'm2.36z0006y24': 5.4127692E+08, 'm2.36z0006y40': 2.4111768E+08}
+customlifetimes = {'m1.7z0006y24':  1.4355016E+09,
+                   'm2.36z0006y24': 5.4127692E+08,
+                   'm3z0006y24':    2.9003497E+08,
+                   'm4z0006y24':    1.4625349E+08,
+                   'm5z0006y24':    8.9311182E+07,
+                   'm6z0006y24':    6.1418488E+07,
+                   'm1.7z0006y40':  5.6321442E+08,
+                   'm2.36z0006y40': 2.4111768E+08,
+                   'm3z0006y40':    1.3478728E+08,
+                   'm4z0006y40':    7.0936165E+07,
+                   'm5z0006y40':    4.4938030E+07,
+                   'm6z0006y40':    3.1594309E+07}
 
 for m in range(len(modelNames)):
     evmodelname = modelNames[m].split('a')[0]
@@ -22,13 +40,18 @@ for m in range(len(modelNames)):
     if evmodelname in customlifetimes:
         lifetime[m] = customlifetimes[evmodelname]
     else:
+        print("Fatal error: Lifetime needed for model '{:}'".format(modelNames[m]))
+        sys.exit()
+        """"
         with open("/Users/lukes/Dropbox/Papers (first author)/2015 He-enhanced IMAGB Stars/generateplots/evolution/" + evmodelname + "/evall.dat", mode='rb') as evfile: #or did you mean evall.dat?
             fileContent = evfile.read()
 
             bytepos = range(20,len(fileContent)-16,268)[-1]
             lifetime[m] = struct.unpack("<d", fileContent[bytepos+4:bytepos+12])[0]
+            #print("                   '{:}':    {:13.7E},".format(evmodelname,lifetime[m]))
+        """
 
-    with open('/Users/lukes/Dropbox/Papers (first author)/2015 He-enhanced IMAGB Stars/generateplots/ppns320species/' + modelNames[m] + '/yields.txt','r') as fyields:
+    with open('data/shinglesetal2015/' + modelNames[m] + '/yields.txt','r') as fyields:
         print("reading " + modelNames[m])
         for line in fyields:
             if not line.startswith("#"):
@@ -70,7 +93,7 @@ with open('data/kobayashi06snyields.txt','r') as k06yields:
 
 with open('yields.txt', 'w') as fileout:
     print("writing yields.txt")
-    fileout.write("#test case: AGB models\n")
+    fileout.write("#test case: Shingles et al. 2015 AGB models and Kobayashi 2006 massive star yields\n")
     fileout.write("#modelname".ljust(25))
     fileout.write("mass".rjust(14))
     fileout.write("Z".rjust(14))
@@ -113,7 +136,7 @@ with open('yields.txt', 'w') as fileout:
             else:
                 yieldout = 0.0
                 if i == k06startindex:
-                    print("Warning '" + species + "' undefined for Kobayashi+2006 models")
+                    print("Warning: '" + species + "' undefined for Kobayashi+2006 models")
             fileout.write(("%14.6e" % yieldout).rjust(14))
 
         fileout.write("\n")
