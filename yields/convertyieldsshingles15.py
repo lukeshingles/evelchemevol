73b62d298276bc6919f609c1e2a52796dfa85a0c
Luke Shingles <luke.shingles@gmail.com>
2016-03-26 19:41:43 +0000

    more pythonic variable names for shingles15 yield converter

--- yields/convertyieldsshingles15.py
+++ yields/convertyieldsshingles15.py
@@ -8,18 +8,18 @@ elements = ['']+[line.split()[1] for line in open('data/atomic_symbols.dat')]
 #(yval, k14alpha, s15alpha) = ('25', '4', '0') #Helium normal
 (yval, k14alpha, s15alpha) = ('40', '0', '0') #Helium rich
 
-modelNames = [s.format(yval,k14alpha,s15alpha) for s in [
+model_names = [s.format(yval,k14alpha,s15alpha) for s in [
                         'm1.7z0006y{0}a{1}pmz001s320',
                         'm2.36z0006y{0}a{1}pmz001s320',
                         'm3z0006y{0}a{2}pmz001s320',
                         'm4z0006y{0}a{2}s320',
                         'm5z0006y{0}a{2}s320',
                         'm6z0006y{0}a{2}s320']]
-yields = [OrderedDict({}) for k in range(len(modelNames))]
-ejectaMass = [0.0 for k in range(len(modelNames))]
-initMass = [float(modelNames[k][1:].split('z')[0]) for k in range(len(modelNames))]
-lifetime = [0.0 for k in range(len(modelNames))]
-metallicity = [0.0 for k in range(len(modelNames))]
+yields = [OrderedDict({}) for k in model_names]
+ejecta_mass = [0.0 for k in model_names]
+init_mass = [float(model_name[1:].split('z')[0]) for model_name in model_names]
+lifetime = [0.0 for k in model_names]
+metallicity = [0.0 for k in model_names]
 
 customlifetimes = {'m1.7z0006y24':  1.4355016E+09,
                    'm2.36z0006y24': 5.4127692E+08,
@@ -34,13 +34,13 @@ customlifetimes = {'m1.7z0006y24':  1.4355016E+09,
                    'm5z0006y40':    4.4938030E+07,
                    'm6z0006y40':    3.1594309E+07}
 
-for m in range(len(modelNames)):
-    evmodelname = modelNames[m].split('a')[0]
+for m,modelname in enumerate(model_names):
+    evmodelname = model_names[m].split('a')[0]
     metallicity[m] = 0.0006
     if evmodelname in customlifetimes:
         lifetime[m] = customlifetimes[evmodelname]
     else:
-        print("Fatal error: Lifetime needed for model '{:}'".format(modelNames[m]))
+        print("Fatal error: Lifetime needed for model '{:}'".format(model_names[m]))
         sys.exit()
         """"
         with open("/Users/lukes/Dropbox/Papers (first author)/2015 He-enhanced IMAGB Stars/generateplots/evolution/" + evmodelname + "/evall.dat", mode='rb') as evfile: #or did you mean evall.dat?
@@ -51,8 +51,8 @@ for m in range(len(modelNames)):
             #print("                   '{:}':    {:13.7E},".format(evmodelname,lifetime[m]))
         """
 
-    with open('data/shinglesetal2015/' + modelNames[m] + '/yields.txt','r') as fyields:
-        print("reading " + modelNames[m])
+    with open('data/shinglesetal2015/' + modelname + '/yields.txt','r') as fyields:
+        print("reading " + modelname)
         for line in fyields:
             if not line.startswith("#"):
                 row = line.split()
@@ -61,17 +61,17 @@ for m in range(len(modelNames)):
                     yields[m][row[0]] = float(row[3]) #absolute yield
                 else:
                     yields[m][row[0]] = float(row[2]) #relative yield
-                ejectaMass[m] += float(row[3])
+                ejecta_mass[m] += float(row[3])
 
 with open('data/kobayashi06snyields.txt','r') as k06yields:
     print("reading kobayashi06snyields.txt")
 
-    k06startindex = len(modelNames)
+    k06startindex = len(model_names)
     k06masslist = [13,15,18,20,25,30,40]
     for mass in k06masslist:
-        modelNames.append('k06-m{0:d}'.format(mass))
-        initMass.append(float(mass))
-        lifetime.append(lifetime[k06startindex-1] * ((mass/initMass[k06startindex-1]) ** (-3.0)))
+        model_names.append('k06-m{0:d}'.format(mass))
+        init_mass.append(float(mass))
+        lifetime.append(lifetime[k06startindex-1] * ((mass/init_mass[k06startindex-1]) ** (-3.0)))
         metallicity.append(0.001)
         yields.append(OrderedDict({}))
 
@@ -79,16 +79,15 @@ with open('data/kobayashi06snyields.txt','r') as k06yields:
         if line.startswith("0.001"):
             row = line.split()
             if row[1] == "M_cut_":
-                for i in range(len(k06masslist)):
-                    ejectaMass.append(k06masslist[i] - float(row[i+2]))
+                for i,k06mass in enumerate(k06masslist):
+                    ejecta_mass.append(k06mass - float(row[i+2]))
             elif row[1] != "M_final_":
-
                 if row[1] in ['p','d']:
                     speciesname = row[1]
                 else:
                     # turn ^26^Mg into mg26
                     speciesname = "".join(reversed(row[1][1:].lower().split('^')))
-                for i in range(len(k06masslist)):
+                for i,k06mass in enumerate(k06masslist):
                     yields[k06startindex+i][speciesname] = float(row[2+i]) #absolute yield
 
 with open('yields.txt', 'w') as fileout:
@@ -100,20 +99,20 @@ with open('yields.txt', 'w') as fileout:
     fileout.write("remnantmass".rjust(14))
     fileout.write("lifetime".rjust(14))
     fileout.write("\n[stellarmodels]\n")
-    fileout.write(str(len(modelNames)) + "\n")
+    fileout.write(str(len(model_names)) + "\n")
 
-    for i in range(len(modelNames)):
-        fileout.write((chr(ord('A') + i) + ':' + modelNames[i]).ljust(25)[:25])
-        fileout.write(("{0:6.2f}".format(initMass[i])).rjust(14))   # mass
+    for i,model_name in enumerate(model_names):
+        fileout.write((chr(ord('A') + i) + ':' + model_names[i]).ljust(25)[:25])
+        fileout.write(("{0:6.2f}".format(init_mass[i])).rjust(14))   # mass
         fileout.write(("{0:6.2e}".format(metallicity[i])).rjust(14))   # metallicity
-        fileout.write(("{0:6.3f}".format((initMass[i]  - ejectaMass[i]))).rjust(14))   # remnant mass
+        fileout.write(("{0:6.3f}".format((init_mass[i]  - ejecta_mass[i]))).rjust(14))   # remnant mass
         fileout.write(("{0:.6e}".format(lifetime[i])).rjust(14))
         fileout.write("\n")
 
     fileout.write("\n" + "#species".ljust(8))
     fileout.write("type".rjust(10))
-    for m in range(len(modelNames)):
-#        shortname = (modelNames[m].split('z')[0] + 'y' + modelNames[m].split('y')[1])
+    for m,model_name in enumerate(model_names):
+#        shortname = (model_names[m].split('z')[0] + 'y' + model_names[m].split('y')[1])
 #        shortname = shortname.split('s')[0]
 #        fileout.write(shortname.rjust(14)[:14])
         fileout.write(chr(ord('A') + m).rjust(14))
@@ -130,9 +129,9 @@ with open('yields.txt', 'w') as fileout:
         else:
             fileout.write("relative".rjust(10))
 
-        for i in range(len(yields)):
-            if species in yields[i]:
-                yieldout = yields[i][species]
+        for i,yields_thismodel in enumerate(yields):
+            if species in yields_thismodel:
+                yieldout = yields_thismodel[species]
             else:
                 yieldout = 0.0
                 if i == k06startindex:
